c4285dfdff183fc2035d2ea135e6803bd3553373
kurs <kursiks@mail.ru>
2025-10-03 22:17:06 +0200

    last fix

--- Package.swift
+++ Package.swift
@@ -7,11 +7,11 @@ let package = Package(
      .iOS(.v14),
    ],
    products: [
-      .library(name: "TranslatorsModule", targets: ["TranslatorsModule"])
+      .library(name: "SmartBookMultiplatform", targets: ["SmartBookMultiplatform"])
    ],
    targets: [
       .binaryTarget(
-         name: "TranslatorsModule",
+         name: "SmartBookMultiplatform",
          url: "https://github.com/KursX/SmartBookMultiplatform/releases/download/v0.1/Multiplatform.xcframework.zip",
          checksum:"fe7cf62f3fe623cdaa44ad0a3e5fd7835921e1135342dcb24fced5b01c7e7223")
    ]
