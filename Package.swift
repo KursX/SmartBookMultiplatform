fa1438d573b48a5cb9f1aa392b080be9cb973802
Stanislau Kurs <kursiks@mail.ru>
2026-03-01 20:10:44 +0100

    5.8

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "SmartBookMultiplatform",
-         url: "https://github.com/KursX/SmartBookMultiplatform/releases/download/v5.7/Multiplatform.xcframework.zip",
-         checksum: "8ce39b9c0f38678026a2a971c33acd23e95c4a786a91345af25e6871cffd2b66")
+         url: "https://github.com/KursX/SmartBookMultiplatform/releases/download/v5.8/Multiplatform.xcframework.zip",
+         checksum: "76f65f7aacd65ab9a7b6cf2876c8adbdc94d38632bea2c1739bdfc4519f426e3")
    ]
 )
