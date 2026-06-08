05433249df77fde760c3789187d0995262141271
Курс Станислав Александрович <skurs@alfabank.ru>
2026-06-08 11:06:38 +0200

    9.6

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "SmartBookMultiplatform",
-         url: "https://github.com/KursX/SmartBookMultiplatform/releases/download/v9.2/Multiplatform.xcframework.zip",
-         checksum: "b78afbccefcf1fe22970c43d9cca87c0e4133905c599a49eea1f00c0fcbf760e")
+         url: "https://github.com/KursX/SmartBookMultiplatform/releases/download/v9.6/Multiplatform.xcframework.zip",
+         checksum: "c0c318dd50d3c15458f031453a762a88b7b87d06567f372c47288c95a1067606")
    ]
 )
